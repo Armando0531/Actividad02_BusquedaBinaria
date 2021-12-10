269063e8d4df90a3066552be58d624e44d8820d8
Armando Enriquez <armandoenriquez0531@gmail.com>
2021-12-09 19:18:03 -0600

    Se creo la clase Menu y sus métodos

--- src/PruebaBusquedas.java
+++ src/PruebaBusquedas.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 class Insercion {
 	
 	public static int[] ordenacionInsercion(int nums[]) {
@@ -18,6 +21,95 @@ class Insercion {
 	}
 	
 }
+
+class Menu{
+	static Scanner input = new Scanner(System.in);
+	
+	public static void impresionNumeros(int[] nums) {
+		System.out.println();
+		
+		int max = nums[0];
+	    for (int a : nums) {
+	        if (a > max)
+	            max = a;
+	    }
+	    int maxS= Integer.toString(max).length();
+		
+		if (nums.length<=900) {
+			for (int i = 0; i < nums.length; i++) {
+				
+				if(i!=0 && i%((int)Math.sqrt(nums.length))==0) {
+					System.out.println();
+				}else if(i==0) {
+					System.out.print("[");
+				}
+				System.out.print(nums[i]);
+				for (int k = 0; k < (maxS-Integer.toString(nums[i]).length()); k++) {
+					System.out.print(' ');
+				}
+				if(i==0) {
+					System.out.print(' ');
+				}else {
+					System.out.print("  ");
+				}
+				
+			}
+		}else {
+			System.out.println("El arreglo es demasiado largo y el menu se puede bugear, solo se van a imprimir 900 muestras");
+			int salto = (int)(nums.length/900);
+			int j = -1;
+			for (int i = (int)(salto/2); i < nums.length; i+=salto) {
+				j+=1;
+				if(i!=0 && j%30==0) {
+					System.out.println();
+				}else if(i==salto) {
+					System.out.print("[");
+				}
+				System.out.print(nums[i]);
+				for (int k = 0; k < (maxS-Integer.toString(nums[i]).length()); k++) {
+					System.out.print(' ');
+				}
+				if(i==0) {
+					System.out.print(' ');
+				}else {
+					System.out.print("  ");
+				}
+				
+			}
+		}
+		System.out.println("]");
+	}
+	
+	public static int validacionNatural() {
+		int ret = 0;
+		boolean err = false;
+		do {
+			try {
+				ret = input.nextInt();
+			} catch (java.util.InputMismatchException e) {
+				System.out.println("entrada no valida, intente de nuevo:");
+				input.nextLine();
+				err=true;
+			}
+			if (ret>0) {
+				err=false;
+			}else {
+				System.out.println("solo números naturales");
+				err=true;
+			}
+		}while(err);
+		return ret;
+	}
+	
+	public static void mostrarMenu(String[] opciones) {
+		System.out.println();
+		for (int i = 0; i < opciones.length; i++) {
+			System.out.println((i+1)+")"+opciones[i]);
+		}
+		System.out.println((opciones.length+1)+")Salir\n");
+	}
+}
+
 public class PruebaBusquedas {
 
 	public static void main(String[] args) {
